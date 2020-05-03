8b5fd118908021c8ac1cb513e8c98fcf661bff8a
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-03 20:27:39 -0300

    Add project files.

---
+++ XadrezConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using XadrezConsole.Tabuleiro;
+
+namespace XadrezConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine($"Posição: {P}");
+        }
+    }
+}
