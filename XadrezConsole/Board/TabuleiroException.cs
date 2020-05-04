61e2778f7f046074f49ecce658b37e48491015c2
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-03 21:11:08 -0300

    Tratando erros com excessão personalizada

---
+++ XadrezConsole/Board/TabuleiroException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XadrezConsole.Board
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string message) : base(message)
+        {
+
+        }
+    }
+}
