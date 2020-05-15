b9e90b4ee559d32d6c989af365ae6984c53ae679
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-14 23:51:32 -0300

    Acrescentando demais peças

---
+++ XadrezConsole/Xadrez/Bispo.cs
@@ -0,0 +1,85 @@
+﻿using XadrezConsole.Board;
+
+namespace XadrezConsole.Xadrez
+{
+    class Bispo : Peca
+    {
+        public Bispo (Tabuleiro tab, Cor cor) : base(cor, tab)
+        {
+
+        }
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Tabuleiro.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // NO
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            while(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if(Tabuleiro.Peca(pos) != null && Tabuleiro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha -= 1;
+                pos.Coluna -= 1;
+            }
+
+            // NE
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            while (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.Peca(pos) != null && Tabuleiro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha -= 1;
+                pos.Coluna += 1;
+            }
+
+            // SE
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            while (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.Peca(pos) != null && Tabuleiro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha += 1;
+                pos.Coluna += 1;
+            }
+
+            // SO
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            while (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.Peca(pos) != null && Tabuleiro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha += 1;
+                pos.Coluna -= 1;
+            }
+
+
+            return mat;
+        }
+
+        public override string ToString()
+        {
+            return "B";
+        }
+    }
+}
